cb0bfc806e2705670969921cd4559e2b144bcee3
Rika <rika.femmi@gmail.com>
2021-10-08 18:39:55 +0200

    *changes to try out coroutines and input

--- BachelorProject/Assets/Scripts/Events and Decks/Event_Manager.cs
+++ BachelorProject/Assets/Scripts/Events and Decks/Event_Manager.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class Event_Manager : MonoBehaviour
 {
     [SerializeField] private List<Event_Deck> event_Decks = new List<Event_Deck>();
+    
 
-    private void Update()
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        StartCoroutine(Test());
+    }
+
+
+    IEnumerator Test()
+    {
+        Debug.Log("hoi");
+        yield return new WaitUntil(KeyPressed);
+
+        if (event_Decks.Count > 0)
         {
-            if (event_Decks.Count > 0)
-            {
-                Event_Deck _Deck = event_Decks[0];
-                Card _Event = _Deck.Draw_Card();
-            }        
+            Event_Deck _Deck = event_Decks[0];
+            Card _Event = _Deck.Draw_Card();
         }
+
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(Test());
+    }
+
+    bool KeyPressed()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            return true;
+
+        else
+            return false;
     }
 }
